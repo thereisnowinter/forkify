8ba149117201ea7dcd5e77b689a67c79d960ce96
thereisnowinter <505975669@qq.com>
2018-06-09 00:42:30 +0800

    finish shopping list

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,10 @@
 import Search from './models/Search'
 import Recipe from './models/Recipe'
+import List from './models/List'
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
+import * as listView from './views/listView'
+
 import {
   elements,
   renderLoader,
@@ -94,11 +97,38 @@ const controlRecipe = async () => {
 
   }
 }
-
 // window.addEventListener('hashchange', controlRecipe)
 // window.addEventListener('load', controlRecipe)
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe))
+
+// list controller
+const controlList = () => {
+  // create a new list if there is none
+  if (!state.list) state.list = new List()
+  // add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient)
+    listView.renderItem(item)
+  })
+}
+
+// handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid
+  console.log(id)
+  // handle the delete button
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+    // delete from state
+    state.list.deleteItem(id)
+    // delete from UI
+    listView.deleteItem(id)
+    // handle the count update
+  } else if (e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10)
+    state.list.updateCount(id, val)
+  }
+})
 
 // handle recipe button clicks
 elements.recipe.addEventListener('click', e => {
@@ -112,13 +142,14 @@ elements.recipe.addEventListener('click', e => {
     // Increase button is clicked
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
-  // } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
-  //   // Add ingredients to shopping list
-  //   controlList();
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    // Add ingredients to shopping list
+    controlList();
   // } else if (e.target.matches('.recipe__love, .recipe__love *')) {
-  //   // Like controller
-  //   controlLike();
-  // }
+  // //   // Like controller
+  // //   controlLike();
+  // // }
   }
   console.log(state.recipe)
 })
+
